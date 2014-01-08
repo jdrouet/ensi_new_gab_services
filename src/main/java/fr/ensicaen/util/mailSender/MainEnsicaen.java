67bfe855a6805a3c2bb3f6161cd52336adb99504
risnax <yassir.bouhaddaoui@gmail.com>
2014-01-08 13:18:54 +0100

    mail sender

---
+++ src/main/java/fr/ensicaen/util/mailSender/MainEnsicaen.java
@@ -0,0 +1,35 @@
+package fr.ensicaen.util.mailSender;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+
+public class MainEnsicaen {
+
+	
+	
+
+
+    public static void main(String[] args) throws AddressException, MessagingException, IOException {
+    	
+    	
+       EnsicaenSender mail = new EnsicaenSender();
+        
+    // attachments
+       String[] attachments = new String[3];
+       attachments[0] = "C:/DP1.pdf";
+       attachments[1] = "C:/Corba-conception-p21.pdf";
+       attachments[2] = "C:/Corba-conception-p22.pdf";
+       
+        String to ="yassir.bouhaddaoui@gmail.com" ; 
+        String sujet = "sujet : balance de l'HTML CSS ici";
+        String messageHTML = "contenu :balance de l'HTML CSS ici";
+
+        mail.EnvoyerMail(to, messageHTML, sujet,attachments);
+      }
+  
+    
+    
+}
