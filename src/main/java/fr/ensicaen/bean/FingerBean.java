5836ec259d9a571343054ee5fb020cb7e4636f00
JMarginier <j.marginier@gmail.com>
2014-01-09 00:12:42 +0100

    Fingerprint

--- src/main/java/fr/ensicaen/bean/FingerBean.java
+++ src/main/java/fr/ensicaen/bean/FingerBean.java
@@ -1,54 +1,52 @@
 package fr.ensicaen.bean;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+
 import fr.ensicaen.entity.Account;
 import fr.ensicaen.entity.Card;
 import fr.ensicaen.entity.Client;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
-import java.io.Serializable;
-
 /**
- * User: Jérémie Drouet
- * Date: 08/01/14
+ * User: Jérémie Drouet Date: 08/01/14
  */
 @ManagedBean
 @SessionScoped
 public class FingerBean extends AbstractBean {
-    private static final long serialVersionUID = 4819179251339653633L;
-    public static final String PAGE = "/pages/finger.xhtml";
+	private static final long serialVersionUID = 4819179251339653633L;
+	public static final String PAGE = "/pages/finger.xhtml";
 
-    @ManagedProperty("#{homeBean}")
-    private HomeBean homeBean;
+	@ManagedProperty("#{homeBean}")
+	private HomeBean homeBean;
 
-    private String result;
+	private String result;
 
-    public HomeBean getHomeBean() {
-        return homeBean;
-    }
+	public HomeBean getHomeBean() {
+		return homeBean;
+	}
 
-    public void setHomeBean(HomeBean homeBean) {
-        this.homeBean = homeBean;
-    }
+	public void setHomeBean(HomeBean homeBean) {
+		this.homeBean = homeBean;
+	}
 
-    public Client getClient() {
-        return this.homeBean.getClient();
-    }
+	public Client getClient() {
+		return this.homeBean.getClient();
+	}
 
-    public Account getAccount() {
-        return this.homeBean.getAccount();
-    }
+	public Account getAccount() {
+		return this.homeBean.getAccount();
+	}
 
-    public Card getCard() {
-        return this.homeBean.getCard();
-    }
+	public Card getCard() {
+		return this.homeBean.getCard();
+	}
 
-    public String getResult() {
-        return result;
-    }
+	public String getResult() {
+		return result;
+	}
 
-    public void setResult(String result) {
-        this.result = result;
-    }
+	public void setResult(String result) {
+		this.result = result;
+	}
 }
