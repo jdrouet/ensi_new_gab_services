0cb16b604294637113417b3073281829ae552ab1
JMarginier <j.marginier@gmail.com>
2014-01-09 22:53:24 +0100

    sign annuler

--- src/main/java/fr/ensicaen/bean/FingerBean.java
+++ src/main/java/fr/ensicaen/bean/FingerBean.java
@@ -1,101 +1,98 @@
 package fr.ensicaen.bean;
 
-import fr.ensicaen.entity.Account;
-import fr.ensicaen.entity.Card;
-import fr.ensicaen.entity.Client;
-import org.primefaces.context.RequestContext;
+import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+
+import fr.ensicaen.entity.Account;
+import fr.ensicaen.entity.Card;
+import fr.ensicaen.entity.Client;
 
 /**
- * User: Jérémie Drouet
- * Date: 08/01/14
+ * User: Jérémie Drouet Date: 08/01/14
  */
 @ManagedBean
 @SessionScoped
 public class FingerBean extends AbstractBean {
-    private static final long serialVersionUID = 4819179251339653633L;
-    public static final String PAGE = "/pages/finger/finger.xhtml";
+	private static final long serialVersionUID = 4819179251339653633L;
+	public static final String PAGE = "/pages/finger/finger.xhtml";
 
-    @ManagedProperty("#{homeBean}")
-    private HomeBean homeBean;
+	@ManagedProperty("#{homeBean}")
+	private HomeBean homeBean;
 
-    private Command command;
-    private int result;
-    private String after;
+	private Command command;
+	private int result;
+	private String after;
 
+	public HomeBean getHomeBean() {
+		return homeBean;
+	}
 
-    public HomeBean getHomeBean() {
-        return homeBean;
-    }
+	public void setHomeBean(HomeBean homeBean) {
+		this.homeBean = homeBean;
+	}
 
-    public void setHomeBean(HomeBean homeBean) {
-        this.homeBean = homeBean;
-    }
+	public Command getCommand() {
+		return command;
+	}
 
-    public Command getCommand() {
-        return command;
-    }
+	public void setCommand(Command command) {
+		this.command = command;
+	}
 
-    public void setCommand(Command command) {
-        this.command = command;
-    }
+	public Client getClient() {
+		return this.homeBean.getClient();
+	}
 
-    public Client getClient() {
-        return this.homeBean.getClient();
-    }
+	public Account getAccount() {
+		return this.homeBean.getAccount();
+	}
 
-    public Account getAccount() {
-        return this.homeBean.getAccount();
-    }
+	public Card getCard() {
+		return this.homeBean.getCard();
+	}
 
-    public Card getCard() {
-        return this.homeBean.getCard();
-    }
+	public int getResult() {
+		return result;
+	}
 
-    public int getResult() {
-        return result;
-    }
+	public void setResult(int result) {
+		this.result = result;
+	}
 
-    public void setResult(int result) {
-        this.result = result;
-    }
+	public String getAfter() {
+		return after;
+	}
 
-    public String getAfter() {
-        return after;
-    }
+	public void setAfter(String after) {
+		this.after = after;
+	}
 
-    public void setAfter(String after) {
-        this.after = after;
-    }
+	public void initialize(Command command, String after) {
+		this.setCommand(command);
+		this.setAfter(after);
+	}
 
-    public void initialize(Command command, String after) {
-        this.setCommand(command);
-        this.setAfter(after);
-    }
+	public void proceed() {
+		if (this.result == 0) {
+			this.getCommand().onSuccess();
+			this.pushInfoNotification("Succes",
+					"Operation effectuée, vous pouvez fermer la fenetre");
+		} else {
+			this.getCommand().onFail();
+			this.pushErrorNotification("Erreur", "Authentification echouée");
+		}
+	}
 
-    public void proceed() {
-        if (this.result == 0) {
-            this.getCommand().onSuccess();
-            this.pushInfoNotification("Succes", "Operation effectuée, vous pouvez fermer la fenetre");
-        } else {
-            this.getCommand().onFail();
-            this.pushErrorNotification("Erreur", "Authentification echouée");
-        }
-    }
+	public static abstract class Command implements Serializable {
+		private static final long serialVersionUID = 4979412193763108182L;
 
-    public static abstract class Command implements Serializable {
-        private static final long serialVersionUID = 4979412193763108182L;
+		public abstract void onSuccess();
 
-        public abstract void onSuccess();
+		public abstract void onFail();
 
-        public abstract void onFail();
-
-    }
+	}
 
 }
--- src/main/java/fr/ensicaen/bean/service/sign/SignatureBean.java
+++ src/main/java/fr/ensicaen/bean/service/sign/SignatureBean.java
@@ -1,132 +1,140 @@
 package fr.ensicaen.bean.service.sign;
 
-import fr.ensicaen.bean.AbstractBean;
-import fr.ensicaen.bean.FingerBean;
-import fr.ensicaen.bean.HomeBean;
-import fr.ensicaen.util.mailSender.EnsicaenSender;
-import fr.ensicaen.util.sign.PdfSign;
-import org.apache.commons.io.IOUtils;
-import org.primefaces.event.FileUploadEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.mail.MessagingException;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.primefaces.event.FileUploadEvent;
+
+import fr.ensicaen.bean.AbstractBean;
+import fr.ensicaen.bean.FingerBean;
+import fr.ensicaen.bean.HomeBean;
+import fr.ensicaen.util.mailSender.EnsicaenSender;
+import fr.ensicaen.util.sign.PdfSign;
 
 /**
- * User: Jérémie Drouet
- * Date: 08/01/14
+ * User: Jérémie Drouet Date: 08/01/14
  */
 @ManagedBean
 @SessionScoped
 public class SignatureBean extends AbstractBean {
-    private static final long serialVersionUID = -3183230042838768890L;
-    private static final String PAGE = "/pages/service/sign/index.xhtml";
+	private static final long serialVersionUID = -3183230042838768890L;
+	private static final String PAGE = "/pages/service/sign/index.xhtml";
 
-    @ManagedProperty("#{homeBean}")
-    private HomeBean homeBean;
+	@ManagedProperty("#{homeBean}")
+	private HomeBean homeBean;
 
-    @ManagedProperty("#{fingerBean}")
-    private FingerBean fingerBean;
+	@ManagedProperty("#{fingerBean}")
+	private FingerBean fingerBean;
 
-    private String mailRecipient;
+	private String mailRecipient;
 
-    private String filename;
+	private String filename;
 
-    private File uploadedFile;
+	private File uploadedFile;
 
-    public String getMailRecipient() {
-        return mailRecipient;
-    }
+	public String getMailRecipient() {
+		return mailRecipient;
+	}
 
-    public void setMailRecipient(String mailRecipient) {
-        this.mailRecipient = mailRecipient;
-    }
+	public void setMailRecipient(String mailRecipient) {
+		this.mailRecipient = mailRecipient;
+	}
 
-    public HomeBean getHomeBean() {
-        return homeBean;
-    }
+	public HomeBean getHomeBean() {
+		return homeBean;
+	}
 
-    public void setHomeBean(HomeBean homeBean) {
-        this.homeBean = homeBean;
-    }
+	public void setHomeBean(HomeBean homeBean) {
+		this.homeBean = homeBean;
+	}
 
-    public FingerBean getFingerBean() {
-        return fingerBean;
-    }
+	public FingerBean getFingerBean() {
+		return fingerBean;
+	}
 
-    public void setFingerBean(FingerBean fingerBean) {
-        this.fingerBean = fingerBean;
-    }
+	public void setFingerBean(FingerBean fingerBean) {
+		this.fingerBean = fingerBean;
+	}
 
-    public String getFilename() {
-        return filename;
-    }
+	public String getFilename() {
+		return filename;
+	}
 
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
 
-    public File getUploadedFile() {
-        return uploadedFile;
-    }
+	public File getUploadedFile() {
+		return uploadedFile;
+	}
 
-    public void setUploadedFile(File uploadedFile) {
-        this.uploadedFile = uploadedFile;
-    }
+	public void setUploadedFile(File uploadedFile) {
+		this.uploadedFile = uploadedFile;
+	}
 
-    public void handleFileUpload(FileUploadEvent event) {
-        this.setFilename(event.getFile().getFileName());
-        File tmp;
-        try {
-            tmp = File.createTempFile("coucou_joan", event.getFile().getFileName());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        tmp.deleteOnExit();
-        try {
-            IOUtils.copy(event.getFile().getInputstream(), new FileOutputStream(tmp));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        this.setUploadedFile(tmp);
-    }
+	public void handleFileUpload(FileUploadEvent event) {
+		this.setFilename(event.getFile().getFileName());
+		File tmp;
+		try {
+			tmp = File.createTempFile("coucou_joan", event.getFile()
+					.getFileName());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		tmp.deleteOnExit();
+		try {
+			IOUtils.copy(event.getFile().getInputstream(),
+					new FileOutputStream(tmp));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		this.setUploadedFile(tmp);
+	}
 
+	public String sendFile() {
+		this.fingerBean.initialize(new FingerBean.Command() {
+			private static final long serialVersionUID = 5665367668948607128L;
 
-    public String sendFile() {
-        this.fingerBean.initialize(new FingerBean.Command() {
-            private static final long serialVersionUID = 5665367668948607128L;
+			@Override
+			public void onSuccess() {
+				sendFileCommand();
+			}
 
-            @Override
-            public void onSuccess() {
-                sendFileCommand();
-            }
+			@Override
+			public void onFail() {
+				// To change body of implemented methods use File | Settings |
+				// File Templates.
+			}
+		}, PAGE);
+		return FingerBean.PAGE;
+	}
 
-            @Override
-            public void onFail() {
-                //To change body of implemented methods use File | Settings | File Templates.
-            }
-        }, PAGE);
-        return FingerBean.PAGE;
-    }
+	public String sendFileCommand() {
+		File signedFile = PdfSign.signPDF(this.getHomeBean().getClient(),
+				this.getUploadedFile());
+		EnsicaenSender sender = new EnsicaenSender();
+		try {
+			sender.sendFile(this.getHomeBean().getClient().getName(),
+					this.mailRecipient, signedFile);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//
+		return HomeBean.PAGE;
+	}
 
-
-    public String sendFileCommand() {
-        File signedFile = PdfSign.signPDF(this.getHomeBean().getClient(), this.getUploadedFile());
-        EnsicaenSender sender = new EnsicaenSender();
-        try {
-            sender.sendFile(this.getHomeBean().getClient().getName(), this.mailRecipient, signedFile);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //
-        return HomeBean.PAGE;
-    }
+	public String onPressCancel() {
+		return HomeBean.PAGE;
+	}
 }
