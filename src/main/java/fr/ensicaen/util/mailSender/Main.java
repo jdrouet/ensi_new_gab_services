a6e08e2693fdb7b5f63102e24ea56b33d0b77142
risnax <yassir.bouhaddaoui@gmail.com>
2014-01-09 13:38:31 +0100

    mail temp

---
+++ src/main/java/fr/ensicaen/util/mailSender/Main.java
@@ -0,0 +1,24 @@
+package fr.ensicaen.util.mailSender;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+public class Main {
+	
+	
+	
+	 public static void main(String[] args) throws AddressException, MessagingException, IOException {
+	    	
+	    	
+         EnsicaenSender mail = new EnsicaenSender();
+           
+          
+          mail.sendFile("Bouhaddaoui Mohamed Yassir","yassir.bouhaddaoui@gmail.com", "fichier", new File("C:/DP1.pdf"));
+        
+      }
+
+}
