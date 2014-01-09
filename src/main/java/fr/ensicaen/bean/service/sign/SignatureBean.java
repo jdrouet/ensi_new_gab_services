2be3048cc63d5772ad3bccacb1c5f31f4d7cf3e4
Jérémie Drouet <jeremie.drouet@gmail.com>
2014-01-09 21:33:29 +0100

    Signature de doc avec empreinte

--- src/main/java/fr/ensicaen/bean/service/sign/SignatureBean.java
+++ src/main/java/fr/ensicaen/bean/service/sign/SignatureBean.java
@@ -1,6 +1,7 @@
 package fr.ensicaen.bean.service.sign;
 
 import fr.ensicaen.bean.AbstractBean;
+import fr.ensicaen.bean.FingerBean;
 import fr.ensicaen.bean.HomeBean;
 import fr.ensicaen.util.mailSender.EnsicaenSender;
 import fr.ensicaen.util.sign.PdfSign;
@@ -23,9 +24,13 @@ import java.io.IOException;
 @SessionScoped
 public class SignatureBean extends AbstractBean {
     private static final long serialVersionUID = -3183230042838768890L;
+    private static final String PAGE = "/pages/service/sign/index.xhtml";
 
     @ManagedProperty("#{homeBean}")
     private HomeBean homeBean;
+
+    @ManagedProperty("#{fingerBean}")
+    private FingerBean fingerBean;
 
     private String mailRecipient;
 
@@ -47,6 +52,14 @@ public class SignatureBean extends AbstractBean {
 
     public void setHomeBean(HomeBean homeBean) {
         this.homeBean = homeBean;
+    }
+
+    public FingerBean getFingerBean() {
+        return fingerBean;
+    }
+
+    public void setFingerBean(FingerBean fingerBean) {
+        this.fingerBean = fingerBean;
     }
 
     public String getFilename() {
@@ -84,7 +97,26 @@ public class SignatureBean extends AbstractBean {
         this.setUploadedFile(tmp);
     }
 
+
     public String sendFile() {
+        this.fingerBean.initialize(new FingerBean.Command() {
+            private static final long serialVersionUID = 5665367668948607128L;
+
+            @Override
+            public void onSuccess() {
+                sendFileCommand();
+            }
+
+            @Override
+            public void onFail() {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        }, PAGE);
+        return FingerBean.PAGE;
+    }
+
+
+    public String sendFileCommand() {
         File signedFile = PdfSign.signPDF(this.getHomeBean().getClient(), this.getUploadedFile());
         EnsicaenSender sender = new EnsicaenSender();
         try {
