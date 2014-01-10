23658bd140bf3c1adc55334e80a29f98abcdb791
JMarginier <j.marginier@gmail.com>
2014-01-10 10:38:02 +0100

    fe

--- src/main/java/fr/ensicaen/bean/HomeBean.java
+++ src/main/java/fr/ensicaen/bean/HomeBean.java
@@ -1,142 +1,145 @@
 package fr.ensicaen.bean;
 
-import java.util.Collections;
-import java.util.List;
+import fr.ensicaen.entity.*;
+import fr.ensicaen.service.IGenericService;
+import fr.ensicaen.service.IServiceService;
+import fr.ensicaen.service.impl.ServiceService;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
-
-import fr.ensicaen.entity.Account;
-import fr.ensicaen.entity.Action;
-import fr.ensicaen.entity.Card;
-import fr.ensicaen.entity.Client;
-import fr.ensicaen.entity.Service;
-import fr.ensicaen.service.IGenericService;
-import fr.ensicaen.service.IServiceService;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * User: Jérémie Drouet Date: 07/01/14
+ * User: Jérémie Drouet
+ * Date: 07/01/14
  */
 @ManagedBean
 @SessionScoped
 public class HomeBean extends AbstractBean {
-	private static final long serialVersionUID = -4523859625680715721L;
-	public static final String PAGE = "/pages/home.xhtml";
+    private static final long serialVersionUID = -4523859625680715721L;
+    public static final String PAGE = "/pages/home.xhtml";
 
-	@ManagedProperty("#{clientService}")
-	private IGenericService<Client> clientService;
+    @ManagedProperty("#{clientService}")
+    private IGenericService<Client> clientService;
 
-	@ManagedProperty("#{actionService}")
-	private IGenericService<Action> actionService;
+    @ManagedProperty("#{actionService}")
+    private IGenericService<Action> actionService;
 
-	@ManagedProperty("#{serviceService}")
-	private IServiceService serviceService;
+    @ManagedProperty("#{serviceService}")
+    private IServiceService serviceService;
 
-	private String info;
+    private String info;
 
-	private String clearPin;
-	private Card card;
+    private String clearPin;
+    private Card card;
 
-	public String getInfo() {
-		return info;
-	}
+    public String getInfo() {
+        try {
+            return info;
+        } finally {
+            this.setInfo(null);
+        }
+    }
 
-	public void setInfo(String info) {
+    public void setInfo(String info) {
+        this.info = info;
+    }
 
-		FacesContext context = FacesContext.getCurrentInstance();
-		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-				info, info));
+    public String getClearPin() {
+        return clearPin;
+    }
 
-		this.info = info;
-	}
+    public void setClearPin(String clearPin) {
+        this.clearPin = clearPin;
+    }
 
-	public String getClearPin() {
-		return clearPin;
-	}
+    public Card getCard() {
+        return card;
+    }
 
-	public void setClearPin(String clearPin) {
-		this.clearPin = clearPin;
-	}
+    public void setCard(Card card) {
+        this.card = card;
+    }
 
-	public Card getCard() {
-		return card;
-	}
+    public Account getAccount() {
+        return this.card.getAccount();
+    }
 
-	public void setCard(Card card) {
-		this.card = card;
-	}
+    public Client getClient() {
+        return this.getAccount().getClient();
+    }
 
-	public Account getAccount() {
-		return this.card.getAccount();
-	}
+    public IGenericService<Action> getActionService() {
+        return actionService;
+    }
 
-	public Client getClient() {
-		return this.getAccount().getClient();
-	}
+    public void setActionService(IGenericService<Action> actionService) {
+        this.actionService = actionService;
+    }
 
-	public IGenericService<Action> getActionService() {
-		return actionService;
-	}
+    public IGenericService<Client> getClientService() {
+        return clientService;
+    }
 
-	public void setActionService(IGenericService<Action> actionService) {
-		this.actionService = actionService;
-	}
+    public void setClientService(IGenericService<Client> clientService) {
+        this.clientService = clientService;
+    }
 
-	public IGenericService<Client> getClientService() {
-		return clientService;
-	}
+    public IServiceService getServiceService() {
+        return serviceService;
+    }
 
-	public void setClientService(IGenericService<Client> clientService) {
-		this.clientService = clientService;
-	}
+    public void setServiceService(IServiceService serviceService) {
+        this.serviceService = serviceService;
+    }
 
-	public IServiceService getServiceService() {
-		return serviceService;
-	}
+    public List<Service> getSelectedServiceList() {
+        if (this.card == null) {
+            return Collections.emptyList();
+        } else if (this.card.getAccount() == null) {
+            return Collections.emptyList();
+        } else if (this.card.getAccount().getClient() == null) {
+            return Collections.emptyList();
+        } else {
+            return this.card.getAccount().getClient().getServiceList();
+        }
+    }
 
-	public void setServiceService(IServiceService serviceService) {
-		this.serviceService = serviceService;
-	}
+    public List<Service> getNonRemovableServicesList() {
+        return this.serviceService.getNonRemovableServices();
+    }
 
-	public List<Service> getSelectedServiceList() {
-		if (this.card == null) {
-			return Collections.emptyList();
-		} else if (this.card.getAccount() == null) {
-			return Collections.emptyList();
-		} else if (this.card.getAccount().getClient() == null) {
-			return Collections.emptyList();
-		} else {
-			return this.card.getAccount().getClient().getServiceList();
-		}
-	}
+    public String getServicePage(Service service) {
+        return "/pages/services/" + service.getPath() + "/index.xhtml";
+    }
 
-	public List<Service> getNonRemovableServicesList() {
-		return this.serviceService.getNonRemovableServices();
-	}
+    public void removeService(Service service) {
+        this.getClient().getServiceList().remove(service);
+        this.clientService.update(this.getClient());
+        this.actionService.add(new Action(this.getClient(), service, false));
+        //
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(
+                null,
+                new FacesMessage(
+                        FacesMessage.SEVERITY_INFO,
+                        "Successful",
+                        service.getName() + " removed"
+                )
+        );
+    }
 
-	public String getServicePage(Service service) {
-		return "/pages/services/" + service.getPath() + "/index.xhtml";
-	}
-
-	public void removeService(Service service) {
-		this.getClient().getServiceList().remove(service);
-		this.clientService.update(this.getClient());
-		this.actionService.add(new Action(this.getClient(), service, false));
-		//
-		FacesContext context = FacesContext.getCurrentInstance();
-		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-				"Successful", service.getName() + " removed"));
-	}
-
-	public String disconnect() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		HttpSession session = (HttpSession) context.getExternalContext()
-				.getSession(false);
-		session.invalidate();
-		return "/pages/disconnect.xhtml";
-	}
+    public String disconnect() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+        session.invalidate();
+        return "/pages/disconnect.xhtml";
+    }
 }
