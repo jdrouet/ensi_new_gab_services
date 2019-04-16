1769a7429c89b252026b74dc4f0f030f7bce181c
ripipoo676 <yassir.bouhaddaoui@gmail.com>
2019-04-16 11:18:44 +0200

    Update Template.java

--- src/main/java/fr/ensicaen/util/generateurBiocode/Template.java
+++ src/main/java/fr/ensicaen/util/generateurBiocode/Template.java
@@ -14,9 +14,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-/** 
-* @author Yassir Mohamed Bouhaddaoui  yassir.bouhaddaoui@gmail.com
-*/
 
 public class Template {
         
